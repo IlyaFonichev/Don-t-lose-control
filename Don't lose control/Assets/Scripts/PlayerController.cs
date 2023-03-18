4295e31cdef35a02f3727e017fef47e81d452c0e
Andryuha <sw3ze.work@gmail.com>
2023-03-18 11:29:28 +0300

    Fix Controller
    Change controller

--- Don't lose control/Assets/Scripts/PlayerController.cs
+++ Don't lose control/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed;
-    private void Update()
+    [SerializeField] private float speed = 10f;
+
+    private Rigidbody2D _rigidBody;
+
+    private void Start()
     {
-        speed = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(5, 0, 0) * speed * Time.deltaTime;
+        _rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        Vector2 direction = new Vector2(moveHorizontal, 0);
+
+        move(direction);
+    }
+
+    private void move(Vector2 direction)
+    {
+        _rigidBody.velocity = direction.normalized * speed;
     }
 }
