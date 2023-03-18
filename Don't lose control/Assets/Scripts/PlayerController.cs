3408119c0ce65135976352a787ad015363affc77
PashtetDev <pashtetdev@gmail.com>
2023-03-18 14:49:20 +0300

    AddWorker

--- Don't lose control/Assets/Scripts/PlayerController.cs
+++ Don't lose control/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed;
+    private float calmness;
+    public static PlayerController instance;
+    private void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+    public float getCalmness
+    {
+        get { return calmness; }
+    }
     private void Update()
     {
         speed = Input.GetAxis("Horizontal");
--- Don't lose control/Assets/Scripts/Worker.cs
+++ Don't lose control/Assets/Scripts/Worker.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class Worker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private List<Sprite> workDisplayes;
+    [SerializeField]
+    private List<Sprite> loafDisplayes;
+    [SerializeField]
+    private GameObject display;
+    [SerializeField]
+    private float workSpeed;
+    [SerializeField]
+    private float myTask;
+    [SerializeField]
+    private WorkerConditions worker;
+    private bool chiefNearby = false;
+    public enum WorkerConditions
     {
-        
+        Work,
+        Loafing,
+        GoHome
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        workSpeed = 0;
+        GetToWork();
+    }
+
+    private void OnMouseDown()
+    {
+        if (chiefNearby)
+        {
+            if (PlayerController.instance.getCalmness > 80)
+            {
+                if (Random.Range(0, 10) == 0)
+                    GoHome();
+                else
+                    GetToWork();
+            }
+            else
+                GetToWork();
+        }
+    }
+    private void Update()
+    {
+        if(worker == WorkerConditions.Work)
+        {
+            if (myTask <= 100)
+                myTask += workSpeed * Time.deltaTime;
+            else
+                GoHome();
+        }
+
+    }
+
+    private void GoHome()
+    {
+        worker = WorkerConditions.GoHome;
+        //Animation
+    }
+    private void GetLoaf()
+    {
+        worker = WorkerConditions.Loafing;
+        display.GetComponent<SpriteRenderer>().sprite = loafDisplayes[Random.Range(0, loafDisplayes.Count)];
+        //Animation
+    }
+    private void GetToWork()
+    {
+        worker = WorkerConditions.Work;
+        display.GetComponent<SpriteRenderer>().sprite = workDisplayes[Random.Range(0, workDisplayes.Count)];
+        //Animation
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Chief"))
+        {
+            chiefNearby = false;
+            GetLoaf();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Chief"))
+            chiefNearby = true; 
+    }
+    public float Task
+    {
+        get { return myTask; }
     }
 }
